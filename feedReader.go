a146bb19efebc8ce2ea78683a979d666509f67ee
Jose Baez <jlb406@gmail.com>
2015-05-19 22:09:38 -0400

    can now return all the episodes in a feed

--- feedReader.go
+++ feedReader.go
@@ -1,14 +1,36 @@
 package feedReader
 
+import (
+	"encoding/xml"
+	"io/ioutil"
+	"net/http"
+)
 
-func GetEpisodesfromFeed(feedUrl string) {
+type FeedXML struct {
+	Title string `xml:"channel>title"`
+	Episodes []EpisodeXML `xml:"channel>item"`
+}
+
+type EpisodeXML struct {
+	Data string `xml:"pubDate"`
+	Description string `xml:"description"`
+	Link string `xml:"link"`
+	Title string `xml:"title"`
+}
+
+func GetEpisodesfromFeed(feedUrl string) ([]EpisodeXML, error) {
 	if resp, err := http.Get(feedUrl); err == nil {
 		if body, err := ioutil.ReadAll(resp.Body); err == nil {
-			log.Println(body)
+			var feed FeedXML
+			if err = xml.Unmarshal([]byte(body), &feed) ; err == nil {
+				return feed.Episodes, nil
+			} else {
+				return nil, err
+			}
 		} else {
-			log.Println(err)
+			return nil, err
 		}
 	} else {
-		log.Println(err)
+		return nil, err
 	}
 }
