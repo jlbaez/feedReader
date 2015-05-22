6cf150512bdf6aee563e1d35dde48c412dc864fd
Jose Baez <jlb406@gmail.com>
2015-05-21 22:02:38 -0400

    can now get more info from feed

--- feedReader.go
+++ feedReader.go
@@ -12,10 +12,16 @@ type FeedXML struct {
 }
 
 type EpisodeXML struct {
-	Data string `xml:"pubDate"`
-	Description string `xml:"description"`
-	Link string `xml:"link"`
-	Title string `xml:"title"`
+	Content string `xml:"content" json:"content"`
+	Date string `xml:"pubDate" json:"date"`
+	Description string `xml:"description" json:"description"`
+	Itunes Itunes `xml:"duration" json:"info"`
+	Link string `xml:"link" json:"link"`
+	Title string `xml:"title" json:"title"`
+}
+
+type Itunes struct {
+	Duration string `xml:",chardata"`
 }
 
 func GetEpisodesfromFeed(feedUrl string) ([]EpisodeXML, error) {
